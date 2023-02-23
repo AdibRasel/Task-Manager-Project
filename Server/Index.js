56bab24509b810883e8b57cd68db5dd30b8193aa
HataillaPola <softzonesapur@gmail.com>
2023-02-23 19:40:34 +0600

    DataBase Connection
    DataBase Connection

--- Server/Index.js
+++ Server/Index.js
@@ -1,5 +1,5 @@
 const App = require("./App.js");
 
 App.listen(5000, ()=>{
-        console.log("Server Run Success Port Address : http://localhost:5000/")
+        console.log("Server Run Success")
 })
